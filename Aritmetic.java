ea112b121f7b2edfe40270ade07a9c3f13aa1173
Garvit Kothari <31393258+Garvit-k@users.noreply.github.com>
2017-12-04 17:30:13 +0530

    Optimization

--- Aritmetic.java
+++ Aritmetic.java
@@ -18,53 +18,51 @@ class Aritmetic
             case 1:
             System.out.println("Enter number of terms to add");
             i=Integer.parseInt(b.readLine());
-            double sum=0;
+            ans=0;
             for(int x=1;x<=i;x++)
             {
                 System.out.println("Enter number to add");
                 n=Double.parseDouble(b.readLine());
-                sum+=n;
+                ans+=n;
             }
-            System.out.println("ANSWER IS "+sum);
             break;
             case 2:
             System.out.println("Enter number of terms to substract");
             i=Integer.parseInt(b.readLine());
-            double diff=0;
+            ans=0;
             for(int x=1;x<=i;x++)
             {
                 System.out.println("Enter number to subtract");
                 n=Double.parseDouble(b.readLine());
-               diff-=n;
+               ans-=n;
             }
-            System.out.println("ANSWER IS  "+diff);
             break;
             case 3:
             System.out.println("Enter number of terms to multiply");
             i=Integer.parseInt(b.readLine());
-            double mul=1;
+            ans=1;
             for(int x=1;x<=i;x++)
             {
                 System.out.println("Enter number to Multiply");
                 n=Double.parseDouble(b.readLine());
-                mul*=n;
+                ans*=n;
             }
-            System.out.println("ANSWER IS  "+mul);
             break;
             case 4:
              System.out.println("Enter number of terms to divide");
             i=Integer.parseInt(b.readLine());
-            double div=1;
+            ans=1;
             for(int x=1;x<=i;x++)
             {
                 System.out.println("Enter number to divide");
                 n=Double.parseDouble(b.readLine());
-                div/=n;
+                ans/=n;
             }
-            System.out.println("ANSWER IS  "+div);
             break;
-            default:System.out.println("WRONG CHOICE");
+            default:
+            System.out.println("WRONG CHOICE");
             break;
         }
+        System.out.println("ANSWER IS  "+ans);
     }
 }
